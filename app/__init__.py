069c9a3f243e24c97c42dc3623b94abc763d1601
Laura de Bella <lauradebella@gmail.com>
2016-11-28 17:40:22 -0200

    Define o ambiente de execução pela variavel de ambiente ENV

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
+from os import getenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask.ext.runner import Manager
 
 flask = Flask(__name__)
-flask.config.from_object('config.Development')
+flask.config.from_object('config.'+ getenv('ENV', 'Development'))
 
 db = SQLAlchemy(flask)
 manager = Manager(flask)
-
