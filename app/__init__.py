51e54ea6914828d2c63ed6659e52189378836fac
Laura de Bella <lauradebella@gmail.com>
2016-11-28 17:46:23 -0200

    Adiciona migrations ao projeto

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from os import getenv
 from flask import Flask
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask.ext.runner import Manager
 
@@ -7,4 +8,5 @@ flask = Flask(__name__)
 flask.config.from_object('config.'+ getenv('ENV', 'Development'))
 
 db = SQLAlchemy(flask)
+migrate = Migrate(flask, db)
 manager = Manager(flask)
--- manage.py
+++ manage.py
@@ -1,3 +1,4 @@
+from flask_migrate import MigrateCommand
 from app import flask, manager
 
 from app.controllers.home_controller import blueprint as home
@@ -6,4 +7,5 @@ from app.controllers.post_controller import blueprint as post
 flask.register_blueprint(home)
 flask.register_blueprint(post)
 
+manager.add_command('db', MigrateCommand)
 manager.run()
