621b465da3acab925525c17b763f0ff76b293ff3
Laura de Bella <lauradebella@gmail.com>
2016-11-28 16:20:54 -0200

    Adiciona conexão com o banco de dados SQLserver

---
+++ Sql/create_post.sql
@@ -0,0 +1,5 @@
+CREATE TABLE POST(
+	id int,
+	title varchar(255),
+	content varchar(4000)
+);
--- manage.py
+++ manage.py
@@ -1,10 +1,37 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, String, Integer
 
 app = Flask(__name__)
+
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = "mssql+pyodbc://sa:131313@localhost:1433/Flask?driver=SQL+Server+Native+Client+11.0"
+
+db = SQLAlchemy(app)
+
+#models
+class Post(db.Model):
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    content = Column(String)
+
+    def __iter__(self):
+        yield 'id', self.id
+        yield 'title', self.title
+        yield 'content', self.content
+
+
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
-if __name__ == "__main__":
-    app.run()
+
+@app.route("/posts")
+def Posts():
+    posts = [dict(d) for d in Post.query.all()]
+    return jsonify(posts=posts), 200
+
+app.run()
+
