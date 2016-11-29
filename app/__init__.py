d302e7be982bc13634e13555385e9255bfb891bf
Laura de Bella <lauradebella@gmail.com>
2016-11-29 18:25:07 -0200

    Adiciona metodo para importar blueprints dinamicamente

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,7 @@
-from os import getenv, getcwd, path
+from os import getenv, listdir, getcwd, path
+from re import sub
+from importlib import import_module
+from inflection import singularize
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -10,3 +13,16 @@ flask.config.from_object('config.'+ getenv('ENV', 'Development'))
 db = SQLAlchemy(flask)
 migrate = Migrate(flask, db)
 manager = Manager(flask)
+
+def register_blueprints(flask, package):
+    package_dir = path.join(getcwd(), __name__, package)
+    module_suffix = '_' + singularize(package) + '.py'
+
+    module_names = [sub('\.py$', '', c)
+                    for c in listdir(package_dir) if c.endswith(module_suffix)]
+
+    for module_name in module_names:
+        module = import_module(__name__ + '.%s.%s' % (package, module_name))
+        flask.register_blueprint(module.blueprint)
+
+register_blueprints(flask, 'controllers')
--- manage.py
+++ manage.py
@@ -1,11 +1,5 @@
 from flask_migrate import MigrateCommand
 from app import flask, manager
 
-from app.controllers.home_controller import blueprint as home
-from app.controllers.post_controller import blueprint as post
-
-flask.register_blueprint(home)
-flask.register_blueprint(post)
-
 manager.add_command('db', MigrateCommand)
 manager.run()
